7a94e929f397454bd4d05c9a762db16a5a3198ab
MagyarL <magyar.levi@gmail.com>
2024-10-31 12:07:51 +0100

    create chatapp project

---
+++ SignalRWebpack/Program.cs
@@ -0,0 +1,27 @@
+using SignalRWebpack.Hubs;
+var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                      policy.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/", () => "Hello World!");
+app.MapHub<ChatHub>("/hub");
+
+app.Run();
+
